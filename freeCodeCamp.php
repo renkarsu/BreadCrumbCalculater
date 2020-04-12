194289ed9ffc10baa5d4af4fbefa6b77592e9337
renkarsu <42439654+renkarsu@users.noreply.github.com>
2020-04-12 22:01:25 +0900

    do php tutotial for 25 minutes from youtube

--- freeCodeCamp.php
+++ freeCodeCamp.php
@@ -6,7 +6,13 @@
     </head>
     <body>
         <?php
-            echo("Hello World");
+            //these functions make a line html code
+            //check your browser, check developer tools or source control
+            echo("Hello World");//same as echo "Hello World";
+
+            echo "<h1>Renkarsu's Site</h1>";
+            echo "<hr>";
+            echo "<p>This is my site</p>";
         ?>
     </body>
 </html>
