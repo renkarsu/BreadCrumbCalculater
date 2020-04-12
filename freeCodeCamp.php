18977f3484a7bfbee5a8c27e6a17d41ff6a37c66
renkarsu <42439654+renkarsu@users.noreply.github.com>
2020-04-12 22:47:51 +0900

    do tutorial for 44:30 from freeCodeCamp

--- freeCodeCamp.php
+++ freeCodeCamp.php
@@ -6,6 +6,7 @@
     </head>
     <body>
         <?php
+        /*writing HTML*/
             //these functions make a line html code
             //check your browser, check developer tools or source control
             echo("Hello World");//same as echo "Hello World";
@@ -14,13 +15,25 @@
             echo "<hr>";
             echo "<p>This is my site</p>";
 
+        /*Variable*/
             $characterName = "John";
             $characterAge = 35;
             //   "There once was a man named John <br>";
             echo "There once was a man named $characterName <br>";
             echo "He was $characterAge years old <br>";
+            //$characterName = "Mike";
             echo "He really liked the name $characterAge <br>";
             echo "But didn't like being $characterAge <br>";
+
+        /*Data types*/
+            $phrase = "To be or not to be";//string, plane text
+            $age = 30;//integers
+            $gpa = 32.98743;//floating number, decimal number
+            $isMale = false;//boolean
+            //null means no value
+            echo 4.57;//show 4.57
+            echp $phrase;
+
         ?>
     </body>
 </html>
