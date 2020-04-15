012c0ef50ccdfeaa9da5482c069aff7ca547be64
renkarsu <42439654+renkarsu@users.noreply.github.com>
2020-04-15 16:19:03 +0900

    make fCC calculator

---
+++ fCC_calculator.php
@@ -0,0 +1,24 @@
+<!--
+    do php tutorial from 1:15:37 to 
+-->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+    <form action = "freeCodeCamp1.php" method = "get">
+        <input type = "number" name = "num1">
+        <br>
+        <input type = "number" name = "num2">
+        <br>
+        <input type = "submit">
+    </form>
+
+    Answer: <?php echo $_GET["num1"] + $_GET["num2"]?>
+
+    </body>
+</html>
